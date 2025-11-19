f2b81ee08277350550771c3c4e586e2799b3f346
mon3stera <mon3stera@proton.me>
2025-11-19 18:27:20 +0800

    fmt(slayerfs): cargo fmt
    Signed-off-by: mon3stera <mon3stera@proton.me>

--- project/slayerfs/src/chuck/chunk.rs
+++ project/slayerfs/src/chuck/chunk.rs
@@ -69,6 +69,12 @@ impl ChunkLayout {
         (offset_in_chunk / self.block_size as u64) as u32
     }
 
+    #[inline]
+    #[allow(dead_code)]
+    pub fn within_block_offset(&self, offset_in_chunk: u64) -> u32 {
+        (offset_in_chunk % self.block_size as u64) as u32
+    }
+
     /// Return the file byte range [start, end) covered by a chunk index (end exclusive).
     #[inline]
     #[allow(dead_code)]
